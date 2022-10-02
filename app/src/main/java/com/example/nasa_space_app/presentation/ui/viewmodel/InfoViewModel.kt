c723da7d88e15517f2ec585f90f27523adb29a38
Mahmoud Elshahatt <79477855+MahmoudElshahatt@users.noreply.github.com>
2022-10-03 01:23:56 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/nasa_space_app/presentation/ui/viewmodel/InfoViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.nasa_space_app.presentation.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class InfoViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
